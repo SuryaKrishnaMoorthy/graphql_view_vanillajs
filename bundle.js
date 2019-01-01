f8bca73ae0b0406fd3928acc538d0ef6965625da
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2019-01-01 13:38:21 -0800

    add voyager element

---
+++ bundle.js
@@ -0,0 +1,1 @@
+console.error("Error: Cannot find module '/Users/surya/workspace/Internship_SC/graphql_view_vanillajs/scripts/main.js' from '/Users/surya/workspace/Internship_SC/graphql_view_vanillajs'");
